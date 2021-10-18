b4e1ee52cf3547ac32042aa456f34cbe53d6b2e4
KwonSongji <mariecamillecalone@gmail.com>
2021-10-18 20:24:48 +0200

    auth error

--- backend/Recap/Methode.md
+++ backend/Recap/Methode.md
@@ -5,3 +5,6 @@ retire les espaces
 ## .unshift():
  
 ajoute ou insère plusieurs élements au tableau 
+
+## .splice() :
+modifie le contenu d'un tableau en retirant des éléments et/ou en ajoutant de 
--- backend/graphql/resolvers/comments.js
+++ backend/graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server')
+const { AuthentificationError, UserInputError } = require('apollo-server')
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -25,6 +25,19 @@ module.exports = {
         return post;
       } else throw new UserInputError('Post not found');
     },
-    async deleteComment(_, { postId})
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        const commentIndex = post.comments.findIndex(c => c.id === commentId);
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthentificationError('Action not allowed')
+        }
+      } else throw new UserInputError('Post not found');
+    }
   }
 }
